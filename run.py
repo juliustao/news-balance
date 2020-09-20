d7f302d7b2d69404d5dc83ec8190fd2cef9fd529
juliustao <juliusltao@gmail.com>
2020-09-20 01:01:12 -0400

    refactor monolithic main method

--- run.py
+++ run.py
@@ -11,6 +11,34 @@ from title import get_title
 from url import get_view
 
 
+def run(driver, opposing, latest):
+    try:
+        current_url = driver.current_url
+    except selenium.common.exceptions.NoSuchWindowException:
+        driver.switch_to.window(driver.window_handles[-1])
+        current_url = driver.current_url
+    split_url = urlsplit(current_url)
+    # check if the site is partisan and if we're not on the home page
+    view = get_view(split_url.netloc)
+    if view and len(split_url.path) > 1:
+        print("DO WE SWITCH?")
+        if opposing:
+            if view == "left":
+                view = "right"
+            elif view == "right":
+                view = "left"
+        else:
+            view = "centrist"
+        title = get_title(current_url)
+        url_to_scrape = get_url_to_scrape(title, view=view, latest=latest)
+        centrist_url = scrape_url(url_to_scrape)
+        if centrist_url != current_url:
+            print("YEP")
+            driver.execute_script("window.open('');")
+            driver.switch_to.window(driver.window_handles[-1])
+            driver.get(centrist_url)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -22,7 +50,7 @@ if __name__ == "__main__":
         help="Time in seconds added between loops.",
     )
     parser.add_argument(
-        "--opposite", action="store_true",
+        "--opposing", action="store_true",
         help="Show articles from opposing rather than centrist views.",
     )
     parser.add_argument(
@@ -38,26 +66,4 @@ if __name__ == "__main__":
     driver = webdriver.Chrome(os.path.join(repo_path, args.driver_path))
     while True:
         time.sleep(args.buffer)
-        try:
-            current_url = driver.current_url
-        except selenium.common.exceptions.NoSuchWindowException:
-            driver.switch_to.window(driver.window_handles[-1])
-            current_url = driver.current_url
-        split_url = urlsplit(current_url)
-        # check if the site is partisan and if we're not on the home page
-        view = get_view(split_url.netloc)
-        if view and len(split_url.path) > 1:
-            if args.opposite:
-                if view == "left":
-                    view = "right"
-                elif view == "right":
-                    view = "left"
-            else:
-                view = "centrist"
-            title = get_title(current_url)
-            url_to_scrape = get_url_to_scrape(title, view=view, latest=args.latest)
-            centrist_url = scrape_url(url_to_scrape)
-            if centrist_url != current_url:
-                driver.execute_script("window.open('');")
-                driver.switch_to.window(driver.window_handles[-1])
-                driver.get(centrist_url)
+        run(driver, args.opposing, args.latest)
