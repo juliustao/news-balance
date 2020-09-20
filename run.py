82602b8071fa60dff498d05fba1e214e29e2c9ff
juliustao <juliusltao@gmail.com>
2020-09-20 00:14:03 -0400

    add option to show opposing side; catch common exceptions

--- run.py
+++ run.py
@@ -1,16 +1,24 @@
+import argparse
 import os
 import time
 from urllib.parse import urlsplit, urlunsplit
 
+import selenium
 from selenium import webdriver
 
 from scrape import get_url_to_scrape, scrape_url
 from title import get_title
-from url import is_partisan
+from url import get_view
+
+opposing_view = {
+    "left": "right",
+    "right": "left",
+}
 
 
 buffer = 1  # in seconds
 driver_path = os.path.join("chromedriver_linux64", "chromedriver")
+show_opposing_view = True
 
 
 if __name__ == "__main__":
@@ -18,12 +26,22 @@ if __name__ == "__main__":
     driver = webdriver.Chrome(os.path.join(repo_path, driver_path))
     while True:
         time.sleep(buffer)
-        current_url = driver.current_url
+        try:
+            current_url = driver.current_url
+        except selenium.common.exceptions.NoSuchWindowException:
+            driver.switch_to.window(driver.window_handles[-1])
+            current_url = driver.current_url
+        except selenium.common.exceptions.WebDriverException:
+            print("Web browser closed: program terminating.")
+            break
         split_url = urlsplit(current_url)
         # check if the site is partisan and if we're not on the home page
-        if is_partisan(split_url.netloc) and len(split_url.path) > 1:
+        view = get_view(split_url.netloc)
+        if view and len(split_url.path) > 1:
+            if show_opposing_view:
+                view = opposing_view[view]
             title = get_title(current_url)
-            url_to_scrape = get_url_to_scrape(title)
+            url_to_scrape = get_url_to_scrape(title, view=view)
             centrist_url = scrape_url(url_to_scrape)
             if centrist_url != current_url:
                 driver.execute_script("window.open('');")
